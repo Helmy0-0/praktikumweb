e5ec91a9559d4889de1896661e6585d1f31ec1b4
Helmy Krisdin Garcia <helmygarcia67@gmail.com>
2025-05-14 20:24:09 +0700

    p

---
+++ BAB5/main.js
@@ -0,0 +1,41 @@
+// function contohlet(){
+//     let y = 10;
+//     if(true){
+//         let y = 20;
+//         console.log("Nilai dalam blok", y);
+//     }
+//     console.log("Nilai diluar blok", y);
+// }
+// contohlet();
+
+// let nilai = 21
+// let nilai2 = 10
+// nilai2 = 1
+// const hasil = nilai + nilai2
+// console.log("hasil = ",hasil)
+
+// let nama = "Helmy"
+// let umur = 20
+// let kelas = "4D Informatika"
+// let panggilan = [
+//     "Helmy",
+//     "Helm",
+//     "Hel",
+// ]
+
+// console.log(nama)
+// alert(`nama saya ${nama}, panggilannya ${panggilan[1]}, umur saya ${umur}, kelas saya ${kelas}`)  
+// console.log(panggilan[0])
+// console.log(panggilan[1])
+// console.log(panggilan[2])
+
+let nama1 = "Budi";
+let umur1 = 30;
+let isStudent = false;
+let buah = ["Apel", "Jeruk", "Mangga"];
+let mahasiswa = {nama: "Rina", usia: 20, Jurusan: "Informatika"};
+
+console.log(nama1);
+console.log(umur1);
+console.log(buah[2]);
+console.log(mahasiswa);
